ffd86cba4a562ae57598992bf2c97b2842ee3912
Breno349 <brenoserrayo10@gmail.com>
2026-07-23 15:15:37 -0300

    pronto: obter informações

--- scrapper_qnotas/index.js
+++ scrapper_qnotas/index.js
@@ -1,11 +1,51 @@
 require('dotenv').config({quiet:true});
 const { Pool } = require('pg');
+const { insertLogin,get_user,get_periodos,get_disciplinas,get_notas,get_user_content } = require('./login.js');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 async function scrapper(page,pool,login,password){
+    try {
 
+        const user_login = await insertLogin(page,login,password);
+        if(user_login.sucess == false){
+            return user_login;
+        }
+
+        const user_info = await get_user(page);
+        if(user_info.sucess == false){
+            return user_info;
+        }
+
+        const user_periodos = await get_periodos(page);
+        if(user_periodos.sucess == false){
+            return user_periodos;
+        }
+
+        const user_diciplinas = await get_disciplinas(page,user_periodos.data[0]);
+        if(user_diciplinas.sucess == false){
+            return user_diciplinas;
+        }
+
+        const user_content = await get_user_content(page,user_info.data,user_diciplinas.data);
+        if(user_content.sucess == false){
+            return user_content;
+        }
+
+        //console.log('user_content: '+JSON.stringify(user_content.data,null,4))
+        const info = JSON.stringify(user_content.data);
+        const base = Buffer.from(info, 'utf-8').toString('base64');
+        console.log('user_content: '+base)
+
+        return user_content;
+
+    } catch (erro){
+        return {
+            sucess: false,
+            message: 'scrapper: '+erro.message
+        }
+    }
 }
 
 async function init(){
@@ -20,11 +60,23 @@ async function init(){
             database: String(process.env.DB_NAME),
             port: process.env.DB_PORT
         });
-        browser = await puppeteer.launch({headless:true});
+        browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+            ],
+        });
 
         const { rows:users } = await pool.query('SELECT * FROM users_qnotas');
         for(const user of users){
-            console.log(JSON.stringify(user,null,2))
+            const page = await browser.newPage();
+            const user_info = await scrapper(page,pool,user.user_login,user.user_senha);
+            if(user_info.sucess == false){
+                console.log(user_info.message)
+            }
+            await page.close();
         }
 
     } catch (erro){
---
+++ scrapper_qnotas/login.js
@@ -0,0 +1,200 @@
+async function insertLogin(page,user,pass){
+    try {
+
+        await page.goto('https://academico.ifes.edu.br/qacademico/index.asp?t=1001', {waitUntil: 'networkidle2'});
+        await page.type('input[id=txtLogin]', user);
+        await page.type('input[id=txtSenha]', pass);
+
+        await Promise.all([
+            page.waitForNavigation({ waitUntil: 'networkidle2' }),
+            page.click('input[name=Submit]')
+        ]);
+
+        const title = (await page.title()).toLowerCase();
+        if(title.includes('acesso negado')){
+            return {
+                sucess: false,
+                message: 'login: acesso negado'
+            }
+        }
+
+        return {
+            sucess: true,
+            message: 'login: login realizado com sucesso'
+        }
+
+    } catch (erro){
+        return {
+            sucess: false,
+            message: 'login: '+erro.message
+        }
+    }
+}
+
+async function get_user(page){
+    try {
+
+        const dados = await page.evaluate(async (url) => {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        }, 'https://academico.ifes.edu.br/webapp/api/autenticacao/usuario-autenticado');
+
+        return {
+            sucess: true,
+            message: 'get_user: dados do usuário obtidos com sucesso',
+            data: dados
+        }            
+    } catch (erro) {
+        return {
+            sucess: false,
+            message: 'get_user: '+erro.message
+        }
+    }
+}
+
+async function get_periodos(page){
+    try {
+
+        const dados = await page.evaluate(async (url) => {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        }, 'https://academico.ifes.edu.br/webapp/api/diarios/aluno/periodos');
+
+        return {
+            sucess: true,
+            message: 'get_periodo: dados do período letivo obtidos com sucesso',
+            data: dados
+        }            
+    } catch (erro) {
+        return {
+            sucess: false,
+            message: 'get_periodo: '+erro.message
+        }
+    }
+}
+
+async function get_disciplinas(page,periodo){
+    try {
+
+        const dados = await page.evaluate(async (url) => {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        }, `https://academico.ifes.edu.br/webapp/api/boletim/disciplinas?anoLetivo=${periodo.anoLetivo}&periodoLetivo=${periodo.periodoLetivo}`);
+
+        return {
+            sucess: true,
+            message: 'get_disciplinas: dados das disciplinas obtidos com sucesso',
+            data: dados
+        }
+    } catch (erro) {
+        return {
+            sucess: false,
+            message: 'get_disciplinas: '+erro.message
+        }
+    }
+}
+
+async function get_notas(page,user,disciplina){
+    try {
+
+        const dados = await page.evaluate(async (url) => {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        }, `https://academico.ifes.edu.br/webapp/api/diarios/aluno/diarios/${disciplina.idDiario}/avaliacoes?idMatricula=${user.idMatricula}`);
+        
+        let pesos;
+        for(const nota of dados){
+            if(nota.descricao === 'Média Nota do Semestre'){
+                pesos = extrair_peso(nota.formula? nota.formula : '');
+                break;
+            }
+        }
+
+        return {
+            sucess: true,
+            message: 'get_notas: dados das notas obtidos com sucesso',
+            data: dados,
+            pesos: pesos
+        }
+    } catch (erro) {
+        return {
+            sucess: false,
+            message: 'get_notas: '+erro.message
+        }
+    }
+}
+
+function extrair_peso(expressao) {
+  const expressaoLimpa = expressao.replace(/['"()]/g, '');
+  const regex = /([A-Z]+[0-9]+)(?:\*([0-9.]+))?/gi;
+  const resultado = {};
+  const matches = expressaoLimpa.matchAll(regex);
+  for (const match of matches) {
+    const multiplicadorRaw = match[2];
+    const nomeAvaliacao = match[1];
+    resultado[nomeAvaliacao] = multiplicadorRaw ? Number(multiplicadorRaw) : 1;
+  }
+  return resultado;
+}
+
+async function get_user_content(page,user,disciplinas){
+    try {
+        const notas_promises = disciplinas.map(async (disciplina) => {
+
+            const avaliacoes = []
+            let lancados = 0;
+
+            const notas = await get_notas(page,user,disciplina);
+            if(notas.sucess == false){
+                console.log(notas.message)
+                return
+            }
+
+            for(const pv of notas.data){
+                if(pv.tipoAvaliacao == 2 || (pv.idEtapa == "3" && pv.nota == null)) continue;
+                const peso = notas.pesos[pv.sigla] ?? 1
+
+                avaliacoes.push({
+                    nome: pv.descricao,
+                    nota: pv.nota? pv.nota*peso : 0,
+                    max: pv.notaMaxima? pv.notaMaxima*peso : 0
+                })
+                lancados += pv.notaMaxima? pv.notaMaxima*peso : 0
+            }
+
+            return {
+                disciplina: disciplina.descricao,
+                professor: disciplina.professor,
+                nota: disciplina.avaliacao? disciplina.avaliacao : 0,
+                lancados: lancados,
+                faltas: disciplina.totalFaltas? disciplina.totalFaltas : 0,
+                avaliacoes: avaliacoes
+            }
+        });
+        const notas = await Promise.all(notas_promises);
+
+        return {
+            sucess: true,
+            message: 'get_user_content: dados do usuário obtidos com sucesso',
+            data: notas
+        }
+    } catch (erro) {
+        return {
+            sucess: false,
+            message: 'get_user_content: '+erro.message
+        }
+    }
+}
+
+module.exports = {
+    insertLogin,
+    get_user,
+    get_periodos,
+    get_disciplinas,
+    get_notas,
+    get_user_content
+}
